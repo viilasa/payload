00ac176e3c7b0adb1e87ada6aaeb323dc168e62d
viilasa <viilasacontact@gmail.com>
2025-04-18 23:37:13 +0530

    Update defaultLexical.ts

--- src/fields/defaultLexical.ts
+++ src/fields/defaultLexical.ts
@@ -16,7 +16,7 @@ export const defaultLexical = lexicalEditor({
     BoldFeature(),
     ItalicFeature(),
     LinkFeature({
-      enabledCollections: ['pages', 'posts'],
+      enabledCollections: ['blogs'],
       fields: ({ defaultFields }) => {
         const defaultFieldsWithoutUrl = defaultFields.filter((field) => {
           if ('name' in field && field.name === 'url') return false
