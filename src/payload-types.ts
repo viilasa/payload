d5a6c1473b721f38d27500ca8d4e04a1cecfcd74
viilasa <viilasacontact@gmail.com>
2025-04-19 14:46:27 +0530

    Update payload-types.ts

--- src/payload-types.ts
+++ src/payload-types.ts
@@ -210,21 +210,7 @@ export interface Service {
 export interface Blog {
   id: number;
   title: string;
-  content: {
-    root: {
-      type: string;
-      children: {
-        type: string;
-        version: number;
-        [k: string]: unknown;
-      }[];
-      direction: ('ltr' | 'rtl') | null;
-      format: 'left' | 'start' | 'center' | 'right' | 'end' | 'justify' | '';
-      indent: number;
-      version: number;
-    };
-    [k: string]: unknown;
-  };
+  content: any;
   excerpt: string;
   featuredImage: number | Media;
   author: number | User;
